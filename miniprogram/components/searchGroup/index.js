cc5d3262c042ad3ab69e865aae53adc374316dcb
ddowrs <whitezyg@gmail.com>
2019-09-28 20:45:59 +0800

    init search group ui style

---
+++ miniprogram/components/searchGroup/index.js
@@ -0,0 +1,17 @@
+// components/searchGroup/index.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {},
+
+  /**
+   * Component initial data
+   */
+  data: {},
+
+  /**
+   * Component methods
+   */
+  methods: {}
+});

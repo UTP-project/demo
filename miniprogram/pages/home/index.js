008ed0029a97a6b6b59f254a54f6e76609f202a2
ddowrs <whitezyg@gmail.com>
2019-10-02 21:20:09 +0800

    feat: add query routes

--- miniprogram/pages/home/index.js
+++ miniprogram/pages/home/index.js
@@ -1,14 +1,22 @@
 // miniprogram/pages/home/index.js
-const { defaultLongitude, defaultLatitude } = require('../../config/map');
+const {
+  amapKey,
+  defaultLongitude,
+  defaultLatitude
+} = require('../../config/map');
+const amapFile = require('../../libs/amap-wx');
 Page({
   /**
    * Page initial data
    */
   data: {
+    loading: false,
+    welcome: true,
     expand: false,
     editMode: false,
     submitDisabled: true,
-    routes: null,
+    routes: {},
+    polyline: [],
     markers: [
       {
         text: '',
@@ -23,6 +31,9 @@ Page({
     longitude: ''
   },
 
+  includePoints: function() {
+    return this.data.markers.filter(el => el.longitude && el.latitude);
+  },
   collapse: function() {
     this.setData({ expand: false });
   },
@@ -31,10 +42,84 @@ Page({
   },
   search: function() {
     if (!this.data.submitDisabled) {
-      const { markers } = this.data;
+      const { markers, routes } = this.data;
+      const polyline = [];
+      const promise = [];
+      const that = this;
+      this.setData({ loading: true });
       for (let i = 0; i < markers.length - 1; i++) {
         for (let j = i + 1; j < markers.length; j++) {
-          console.log(markers[i], markers[j]);
+          const origin = `${markers[i].longitude},${markers[i].latitude}`;
+          const destination = `${markers[j].longitude},${markers[j].latitude}`;
+          promise.push(
+            new Promise((resolve, reject) => {
+              that.amap.getDrivingRoute({
+                origin,
+                destination,
+                success: function(data) {
+                  const route = {};
+                  const points = [];
+                  if (data.paths && data.paths[0] && data.paths[0].steps) {
+                    const steps = data.paths[0].steps;
+                    for (let k = 0; k < steps.length; k++) {
+                      const poLen = steps[k].polyline.split(';');
+                      for (let l = 0; l < poLen.length; l++) {
+                        const point = {
+                          longitude: parseFloat(poLen[l].split(',')[0]),
+                          latitude: parseFloat(poLen[l].split(',')[1])
+                        };
+                        points.push(point);
+                      }
+                    }
+                  }
+                  if (data.paths[0] && data.paths[0].duration) {
+                    route.duration = +data.paths[0].duration;
+                  }
+                  if (data.paths[0] && data.paths[0].distance) {
+                    route.distance = +data.paths[0].distance;
+                  }
+                  if (data.taxi_cost) {
+                    route.cost = parseInt(data.taxi_cost);
+                  }
+                  polyline.push({
+                    points,
+                    color: '#00B2B2',
+                    width: 6
+                  });
+                  routes[`${markers[i].id},${markers[j].id}`] = route;
+                  that.setData({
+                    polyline,
+                    routes
+                  });
+                  resolve();
+                },
+                fail: function(info) {
+                  reject(info);
+                }
+              });
+            })
+          );
+          Promise.all(promise).then(() => {
+            const { polyline } = this.data;
+            const points = [];
+            let i = 0;
+            while (i < polyline.length) {
+              if (Array.isArray(polyline[i].points)) {
+                points.push(...polyline[i].points);
+              }
+              i++;
+            }
+            this.mapCtx.includePoints({
+              points,
+              padding: [40, 40, 40, 40]
+            });
+            this.setData({
+              loading: false,
+              expand: false,
+              editMode: false,
+              welcome: false
+            });
+          });
         }
       }
     }
@@ -112,7 +197,10 @@ Page({
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function() {},
+  onReady: function() {
+    this.mapCtx = wx.createMapContext('home-map');
+    this.amap = new amapFile.AMapWX({ key: amapKey });
+  },
 
   /**
    * Lifecycle function--Called when page show
